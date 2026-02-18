4405e4baa90d6639e1227287a046bfe02e3538b7
dalthonmh <dalthonmh@gmail.com>
2026-02-17 21:09:21 -0500

    feat: add version to create RedHat version

--- virtualbox/create-vm.sh
+++ virtualbox/create-vm.sh
@@ -2,12 +2,12 @@
 # Creation: 2025-10-30
 # Author: dalthonmh
 
-# Note: "spacex" is the name of the VM we are going to create.
+# Note: "newshepard" is the name of the VM we are going to create.
 
 # -----------------------------------------------
 # 1. Verify the name of the Wifi network adapter
 VBoxManage list bridgedifs | grep -i name
-# In the VBoxManage modifyvm "spacex" command, replace the bridgeadapter1 name with the Wifi adapter name.
+# In the VBoxManage modifyvm "newshepard" command, replace the bridgeadapter1 name with the Wifi adapter name.
 # Example: "--bridgeadapter1 "en0: Wi-Fi""
 # -----------------------------------------------
 
@@ -15,10 +15,11 @@ VBoxManage list bridgedifs | grep -i name
 
 # -----------------------------------------------
 # 2. Create the VM in VirtualBox
-VBoxManage createvm --name "spacex" --ostype "Debian_64" --register
+#DEBIAN VBoxManage createvm --name "newshepard" --ostype "Debian_64" --register
+#REDHAT VBoxManage createvm --name "newshepard" --ostype "RedHat_64" --register
 
 # General configuration
-VBoxManage modifyvm "spacex" \
+VBoxManage modifyvm "newshepard" \
   --memory 2048 \
   --cpus 2 \
   --vram 128 \
@@ -32,25 +33,28 @@ VBoxManage modifyvm "spacex" \
   --cableconnected1 on
 
 # Create and attach a virtual hard disk
-VBoxManage createhd --filename "$HOME/VirtualBox VMs/spacex/spacex.vdi" --size 20000 --format VDI
-VBoxManage storagectl "spacex" --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach "spacex" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/spacex/spacex.vdi"
+VBoxManage createhd --filename "$HOME/VirtualBox VMs/newshepard/newshepard.vdi" --size 20000 --format VDI
+VBoxManage storagectl "newshepard" --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach "newshepard" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/newshepard/newshepard.vdi"
 
 # Attach the installation ISO
-VBoxManage storagectl "spacex" --name "IDE Controller" --add ide
-VBoxManage storageattach "spacex" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "/Users/dalthon/Documents/iso/debian-13-amd64-netinst.iso"
+VBoxManage storagectl "newshepard" --name "IDE Controller" --add ide
+#DEBIAN VBoxManage storageattach "newshepard" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "/Users/dalthon/Documents/iso/debian-13-amd64-netinst.iso"
+#REDHAT VBoxManage storageattach "newshepard" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "/Users/dalthon/Documents/iso/almalinux-10.1-x86_64-minimal.iso"
 
 # Configure boot order
-VBoxManage modifyvm "spacex" --boot1 dvd --boot2 disk
+VBoxManage modifyvm "newshepard" --boot1 dvd --boot2 disk
 
 # Start the virtual machine
-VBoxManage startvm "spacex" --type gui
+VBoxManage startvm "newshepard" --type gui
 # -----------------------------------------------
 
 
 
 # -----------------------------------------------
 # 3. Delete the virtual machine
-VBoxManage controlvm "spacex" poweroff
-VBoxManage unregistervm "spacex" --delete
+VBoxManage controlvm "newshepard" poweroff
+VBoxManage unregistervm "newshepard" --delete
+
+#rm -rf "$HOME/VirtualBox VMs/newshepard"
 # -----------------------------------------------
